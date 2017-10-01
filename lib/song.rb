05fbd8360306e34ec4f608a7ba588d765ba576dc
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-01 17:11:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 class Song
 
-  extend Memorable 
-  
+  extend Memorable
+
   attr_accessor :name
   attr_reader :artist
 
@@ -33,7 +33,7 @@ class Song
 #    self.all.count
 #  end
 
-#  def self.all
-#    @@songs
-#  end
+  def self.all
+    @@songs
+  end
 end
