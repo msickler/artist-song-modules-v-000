b7f495f6c05d752d16463b8a1208bc59a48f43ab
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-01 17:30:44 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,6 @@
 module Memorable
 
+module ClassMethods
   def count
     all.count
   end
@@ -7,5 +8,12 @@ module Memorable
   def reset_all
     all.clear
   end
+end
+
+module InstanceMethods
+  def initialize
+  end
+
+end
 
 end
